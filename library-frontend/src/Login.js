328f33fbd40723a0d00274e830a691e30989e317
Lombame Lemma <lombamelemma@gmail.com>
2026-02-24 19:40:29 +0300

    login

---
+++ library-frontend/src/Login.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import API from "../api/axiosConfig";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await API.post("/auth/login", { email, password });
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("role", res.data.role); // admin or student
+
+      if (res.data.role === "admin") navigate("/admin");
+      else navigate("/student");
+    } catch (err) {
+      alert(err.response.data.message || "Login failed");
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <h2>Login</h2>
+      <input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} required />
+      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} required />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
